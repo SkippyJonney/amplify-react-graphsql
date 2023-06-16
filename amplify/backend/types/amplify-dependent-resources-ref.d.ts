dd3bec21db3fe10c814a38ec29a738e4cdebe6a2
HYPERION\Jon <jodgarci@gmail.com>
2023-06-16 05:53:27 -0700

    added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphsql4e47a013": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './Boho Sea Turtle.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css"
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+
+function App({ signOut }) {
   return (
     <div className="App">
+      <Card>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
       <header className="App-header">
+
+
         <h1>Hello My Love!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +36,12 @@ function App() {
           Come visit me @Facebook
         </a>
       </header>
+      <body>
+        <h2>Body Header</h2>
+        <p>Body Paragraph</p>
+      </body>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
